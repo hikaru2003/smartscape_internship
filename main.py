bb96d8c64f0caf73cb263e07749015c0492eaf39
hikaru2003 <hikaru.morisaki.0316@gmail.com>
2024-08-07 17:57:42 +0900

    [update] main.py

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 import cv2
 import os
 import numpy as np
-from IPython import display
-import tkinter as tk
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-from adjust_light import histgram_equalize, adjust_brightness, adjust_image
-from split_image import split_image
-from merge_images import merge_images
-from smoothing import smoothing
-from homography import get_sift_matches, find_homography_opencv,\
-    apply_homography, blend_image, homography
+from src.adjust_light import histgram_equalize, adjust_brightness, adjust_image, adjust_images
+from src.split_image import split_image
+from src.merge_images import merge_images
+from src.smoothing import smoothing
+from src.homography import homography
 
 dirname = os.path.dirname(__file__)
+file1 = 'difficult_1.png'
+file2 = 'difficult_2.png'
 
 def diff_img(img1, img1_gray, img2_gray):
     # 画像サイズの調整
     img1_gray = cv2.resize(img1_gray, (img2_gray.shape[1], img2_gray.shape[0]))
+    img1 = cv2.resize(img1, (img2_gray.shape[1], img2_gray.shape[0]))
     # 画像を引き算
     img_diff = cv2.absdiff(img1_gray, img2_gray)
     cv2.imwrite(dirname + '/output/img_diff.png', img_diff)
     # 2値化
     ret2, img_th = cv2.threshold(img_diff, 40, 255, cv2.THRESH_BINARY)
+    img_th_rgb = cv2.cvtColor(img_th, cv2.COLOR_GRAY2RGB)
+    img_th_rgb = np.where(img_th_rgb == (255, 255, 255), (0, 0, 255), img1)
     cv2.imwrite(dirname + '/output/img_th.png', img_th)
+    cv2.imwrite(dirname + '/output/diff_filled.png', img_th_rgb)
     # 輪郭を検出
     contours, hierarchy = cv2.findContours(img_th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     cv2.drawContours(img1, contours, -1, (0, 0, 255), 2)
@@ -59,10 +62,10 @@ def main():
     # 画像読み込み
     # file1 = input('file1: ')
     # file2 = input('file2: ')
-    file1 = 'image3.png'
-    file2 = 'image4.png'
     img_1 = cv2.imread(dirname + '/sample/' + file1)
     img_2 = cv2.imread(dirname + '/sample/' + file2)
+    cv2.imwrite(dirname + '/output/sample1.png', img_1)
+    cv2.imwrite(dirname + '/output/sample2.png', img_2)
     IMG_1 = img_1
     IMG_2 = img_2
 
@@ -78,38 +81,45 @@ def main():
     img_1 = cv2.resize(img_1, (int(width), int(height)))
     img_2 = cv2.resize(img_2, (int(width), int(height)))
     
-    # グレースケール変換
-    # img_1_gray = cv2.cvtColor(img_1, cv2.COLOR_BGR2GRAY)
-    # img_2_gray = cv2.cvtColor(img_2, cv2.COLOR_BGR2GRAY)
-    
     # ホモグラフィ変換
     # img_2_gray = homography(img_2, img_1, n_matches, max_iters)
     img_2 = homography(img_2, img_1, n_matches, max_iters)
     cv2.imwrite(dirname + '/output/homo_2.png', img_2)
     mask = create_mask(img_2)
-    # img_1 = cv2.resize(img_1, (mask.shape[1], mask.shape[0]))
     img_1 = cv2.bitwise_and(img_1, mask)
+    cv2.imwrite(dirname + '/output/homo_1.png', img_1)
     
     # imgを横にhor個, 縦にver個に分割する
+    ver = 8
     hor = 4
-    ver = 3
+    print('vertical', ver)
+    print('horizontal', hor)
     split_img_1 = split_image(hor, ver, img_1)
     split_img_2 = split_image(hor, ver, img_2)
 
     # ヒストグラム平均化 二つの画像で大きく異なるので、逆に精度が落ちる
-    # img_1_gray = histgram_equalize(img_1_gray)
-    # img_2_gray = histgram_equalize(img_2_gray)
-    # cv2.imwrite(dirname + '/output/hist_1.png', img_1_gray)
-    # cv2.imwrite(dirname + '/output/hist_2.png', img_2_gray)
+    # split_img_1 = histgram_equalize(split_img_1, ver*hor)
+    # split_img_2 = histgram_equalize(split_img_2, ver*hor)
     
-    # 平均明度調整
-    # 明度が高いほうの画像を低いほうの画像に合わせる <- そのほうが明るさの違いによる誤差分が小さくなる
-    img1_mean = np.mean(cv2.cvtColor(img_1, cv2.COLOR_RGB2GRAY))
-    img2_mean = np.mean(cv2.cvtColor(img_2, cv2.COLOR_RGB2GRAY))
-    if (img1_mean < img2_mean):
-        split_img_2 = adjust_image(split_img_1, split_img_2, ver*hor)
-    else:
-        split_img_1 = adjust_image(split_img_2, split_img_1, ver*hor)
+    # 平均明度調整 <- 明度が高いほうの画像を低いほうの画像に合わせる <- そのほうが明るさの違いによる誤差分が小さくなる
+    # 画像全体の明度が高いほうの画像を低いほうの画像に合わせる
+    # img1_mean = np.mean(cv2.cvtColor(img_1, cv2.COLOR_RGB2GRAY))
+    # img2_mean = np.mean(cv2.cvtColor(img_2, cv2.COLOR_RGB2GRAY))
+    # if (img1_mean < img2_mean):
+    #     split_img_2 = adjust_images(split_img_1, split_img_2, ver*hor)
+    # else:
+    #     split_img_1 = adjust_images(split_img_2, split_img_1, ver*hor)
+
+    # 分割した各画像に対して明度が高いほうを低いほうの画像に合わせる(分割したすべての部分において明るいほうを暗いほうに合わせるのでより精度が高くなる)
+    for i in range(ver*hor):
+        img1_mean = np.mean(cv2.cvtColor(split_img_1[i], cv2.COLOR_RGB2GRAY))
+        img2_mean = np.mean(cv2.cvtColor(split_img_2[i], cv2.COLOR_RGB2GRAY))
+        if (img1_mean < img2_mean):
+            split_img_2[i] = adjust_image(split_img_1[i], split_img_2[i])
+            print('1')
+        else:
+            split_img_1[i] = adjust_image(split_img_2[i], split_img_1[i])
+            print('2')
 
     # 画像の結合
     img_1 = merge_images(hor, ver, split_img_1)
@@ -119,9 +129,20 @@ def main():
     img_2_gray = cv2.cvtColor(img_2, cv2.COLOR_BGR2GRAY)
     
     # smoothing
+    # バイラテラルフィルタ
+    # for i in range(3):
+    #     img_1_gray = cv2.bilateralFilter(img_1_gray, 15, 20, 20)
+    #     img_2_gray = cv2.bilateralFilter(img_2_gray, 15, 20, 20)
+    # ノンローカルミーんフィルタ
+    img_1 = cv2.fastNlMeansDenoisingColored(img_1,None,10,10,7,21)
+    img_2 = cv2.fastNlMeansDenoisingColored(img_2,None,10,10,7,21)
+    img_1_gray = cv2.cvtColor(img_1, cv2.COLOR_BGR2GRAY)
+    img_2_gray = cv2.cvtColor(img_2, cv2.COLOR_BGR2GRAY)
+    
+    # smoothingの最後にsmoothing()を呼ばないと、画像端の切り取った部分が差分として認識されてしまう
     img_1_gray, img_2_gray = smoothing(img_1_gray, img_2_gray, height, width)
-    cv2.imwrite(dirname + '/output/blur_1.jpg', img_1_gray)
-    cv2.imwrite(dirname + '/output/blur_2.jpg', img_2_gray)
+    cv2.imwrite(dirname + '/output/blur_1.png', img_1_gray)
+    cv2.imwrite(dirname + '/output/blur_2.png', img_2_gray)
 
     # ２画像の差異を計算
     # img_1 = cv2.resize(img_1, (img_2.shape[1], img_2.shape[0]))
