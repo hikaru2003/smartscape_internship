7090aa2ebe9edcd4454fc245b5ed8355f1de8274
hikaru2003 <hikaru.morisaki.0316@gmail.com>
2024-08-08 11:30:31 +0900

    [update] smoothing.py カーネルサイズの変更

--- src/smoothing.py
+++ src/smoothing.py
@@ -8,6 +8,10 @@ def smoothing(img1, img2, height, width):
     # wsize = 5
     hsize = hsize if hsize % 2 == 1 else hsize + 1
     wsize = wsize if wsize % 2 == 1 else wsize + 1
+    # if (hsize > 51): hsize = 51
+    # if (wsize > 51): wsize = 51
+    # hsize = 11
+    # wsize = 11
     print('hsize', hsize)
     print('wsize', wsize)
     return cv2.GaussianBlur(img1,(wsize, hsize),0),\
